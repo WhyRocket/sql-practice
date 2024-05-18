633c495e735742177757cdfe36861257692648c3
WhyRocket <160044031+WhyRocket@users.noreply.github.com>
2024-05-19 01:34:22 +0500

    Внес правки

--- src/1_init.sql
+++ src/1_init.sql
@@ -5,22 +5,23 @@ CREATE SCHEMA library;
 CREATE TABLE library.books
 (
     id SERIAL PRIMARY KEY,
-    title VARCHAR(1000),
-    publishing_year DATE,
-    isbn CHAR(17),
-    publisher_id SERIAL,
-    age_limit SMALLINT,
-    books_number SMALLINT
+    title VARCHAR(1000) NOT NULL,
+    publishing_year DATE NOT NULL,
+    isbn CHAR(17) NOT NULL,
+    publisher_id INTEGER NOT NULL,
+    age_limit SMALLINT NOT NULL,
+    books_number SMALLINT NOT NULL
 );
 
 CREATE TABLE library.genres
 (
     id SERIAL PRIMARY KEY,
-    genre VARCHAR(20)
+    "name" VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE library.book_genres
 (
-    id_genre INTEGER REFERENCES genres (Id) ON DELETE SET NULL,
-    id_book INTEGER REFERENCES books (Id) ON DELETE CASCADE
+    id_genre INTEGER REFERENCES genres (Id) ON DELETE RESTRICT,
+    id_book INTEGER REFERENCES books (Id) ON DELETE CASCADE,
+    PRIMARY KEY (id_genre, id_book)
 );
