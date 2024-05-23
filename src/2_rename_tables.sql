b8fb4363b4afc2f6651cc7e35a1066dca971a6e4
WhyRocket <160044031+WhyRocket@users.noreply.github.com>
2024-05-23 21:06:56 +0500

    Исправил ошибки и изменил название файла

---
+++ src/2_rename_tables.sql
@@ -0,0 +1,11 @@
+ALTER TABLE library.customer
+RENAME TO customers;
+
+ALTER TABLE library.publisher
+RENAME TO publishers;
+
+ALTER TABLE library.author
+RENAME TO authors;
+
+ALTER TABLE library.country
+RENAME TO countries;
