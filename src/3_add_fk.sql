24e4968d01b8124cb6c63e9d8b9bb4b9033a20ec
WhyRocket <160044031+WhyRocket@users.noreply.github.com>
2024-05-24 20:10:47 +0500

    Исправление ошибки

--- src/3_add_fk.sql
+++ src/3_add_fk.sql
@@ -1,5 +1,5 @@
 ALTER TABLE library.publishers
-ADD FOREIGN KEY (id_country) REFERENCES country (id);
+ADD FOREIGN KEY (id_country) REFERENCES countries (id);
 
 ALTER TABLE library.authors
-ADD FOREIGN KEY (id_country) REFERENCES country (id);
+ADD FOREIGN KEY (id_country) REFERENCES countries (id);
